5b7e930808ad2a6c96ea3ef849c9d7363db08e0f
vjardimb <vitorjardimbueno@hotmail.com>
2024-02-06 02:09:20 +0000

    first working agent

--- SimplebikeResetFcn.m
+++ SimplebikeResetFcn.m
@@ -50,13 +50,9 @@ time = 0:1:(length(transformedPath)-1);
 ref_x_ts = timeseries(transformedPath(:, 1), time, 'Name', 'X Reference');
 ref_y_ts = timeseries(transformedPath(:, 2), time, 'Name', 'Y Reference');
 
-% Generate a speed reference between disired limits
-ref_v = ref_v_lims(1) + (ref_v_lims(2) - ref_v_lims(1))*rand(1,1);
-
 % Update variables
 in = in.setVariable('ref_x',transformedPath(:, 1));
 in = in.setVariable('ref_y',transformedPath(:, 2));
 in = in.setVariable('ref_theta',unwrap(orientationReference));
 in = in.setVariable('ref_x_ts',ref_x_ts);
 in = in.setVariable('ref_y_ts',ref_y_ts);
-in = in.setVariable('ref_v',ref_v);
